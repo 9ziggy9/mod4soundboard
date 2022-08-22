60ffdbf95e55cf0e9df144f1ea79393a8c517d1e
garydsong <ceouls@gmail.com>
2022-08-22 14:01:26 -0700

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+function playclip() {
+  let audio = document.getElementsByTagName("audio")[0];
+  audio.play();
+}
+
+function playclip2() {
+  let audio = document.getElementsByTagName("audio")[1];
+  audio.play();
+}
+
+function playclip3() {
+  let audio = document.getElementsByTagName("audio")[2];
+  audio.play();
+}
+
+function playclip4() {
+  let audio = document.getElementsByTagName("audio")[3];
+  audio.play();
+}
+
+function playclip5() {
+  let audio = document.getElementsByTagName("audio")[4];
+  audio.play();
+}
+
+function playclip6() {
+  let audio = document.getElementsByTagName("audio")[5];
+  audio.play();
+}
+
+function playclip7() {
+  let audio = document.getElementsByTagName("audio")[6];
+  audio.play();
+}
+
+function playclip8() {
+  let audio = document.getElementsByTagName("audio")[7];
+  audio.play();
+}
+
+function playclip9() {
+  let audio = document.getElementsByTagName("audio")[8];
+  audio.play();
+}
+
+function playclip10() {
+  let audio = document.getElementsByTagName("audio")[9];
+  audio.play();
+}
